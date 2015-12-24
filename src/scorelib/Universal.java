144cb5bcf7a268be8107e13b1549eda83e07494e
Vance Turnewitsch <vance.turner1@gmail.com>
2015-12-23 19:44:50 -0500

    Universal constants added

---
+++ src/scorelib/Universal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scorelib;
+
+/**
+ *
+ * @author vance
+ */
+public class Universal {
+    
+    public static Integer MAX_INTERVAL = 6;
+    public static Integer INTERVAL_LINES = 4;
+}
